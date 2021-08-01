5fa73e37319de9dd48a32d520fd0e3e26ebfc249
Laura <60604372+lauramurgatroyd@users.noreply.github.com>
2021-08-01 18:09:31 +0100

    Improved song

--- main.ts
+++ main.ts
@@ -1,17 +1,24 @@
 let moving = 0
 input.onButtonPressed(Button.A, function () {
-    basic.showLeds(`
-        . . . . .
-        . # . # .
-        . . . . .
-        # . . . #
-        . # # # .
-        `)
     if (moving == 1) {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . . . .
+            # . . . #
+            . # # # .
+            `)
         kitronik_klip_motor.motorOff(kitronik_klip_motor.Motors.Motor1)
         kitronik_klip_motor.motorOff(kitronik_klip_motor.Motors.Motor2)
         moving = 0
     } else {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . . . .
+            # . . . #
+            . # # # .
+            `)
         kitronik_klip_motor.motorOn(kitronik_klip_motor.Motors.Motor1, kitronik_klip_motor.MotorDirection.Forward, 100)
         kitronik_klip_motor.motorOn(kitronik_klip_motor.Motors.Motor2, kitronik_klip_motor.MotorDirection.Forward, 100)
         moving = 1
@@ -19,29 +26,37 @@ input.onButtonPressed(Button.A, function () {
 })
 input.onButtonPressed(Button.B, function () {
     music.playTone(262, music.beat(BeatFraction.Half))
+    basic.pause(10)
     music.playTone(262, music.beat(BeatFraction.Half))
+    basic.pause(10)
     music.playTone(294, music.beat(BeatFraction.Whole))
     music.playTone(262, music.beat(BeatFraction.Whole))
     music.playTone(349, music.beat(BeatFraction.Whole))
     music.playTone(330, music.beat(BeatFraction.Double))
     basic.pause(10)
     music.playTone(262, music.beat(BeatFraction.Half))
+    basic.pause(10)
     music.playTone(262, music.beat(BeatFraction.Half))
+    basic.pause(10)
     music.playTone(294, music.beat(BeatFraction.Whole))
     music.playTone(262, music.beat(BeatFraction.Whole))
     music.playTone(392, music.beat(BeatFraction.Whole))
     music.playTone(349, music.beat(BeatFraction.Double))
     basic.pause(10)
     music.playTone(262, music.beat(BeatFraction.Half))
+    basic.pause(10)
     music.playTone(262, music.beat(BeatFraction.Half))
+    basic.pause(10)
     music.playTone(523, music.beat(BeatFraction.Whole))
     music.playTone(440, music.beat(BeatFraction.Whole))
     music.playTone(349, music.beat(BeatFraction.Whole))
     music.playTone(330, music.beat(BeatFraction.Whole))
-    music.playTone(294, music.beat(BeatFraction.Whole))
+    music.playTone(294, music.beat(BeatFraction.Double))
     basic.pause(10)
     music.playTone(466, music.beat(BeatFraction.Half))
+    basic.pause(10)
     music.playTone(466, music.beat(BeatFraction.Half))
+    basic.pause(10)
     music.playTone(440, music.beat(BeatFraction.Whole))
     music.playTone(349, music.beat(BeatFraction.Whole))
     music.playTone(392, music.beat(BeatFraction.Whole))
