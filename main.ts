aa735f3d823614b80e4b8967c2d4fbb5ad19816c
Laura <60604372+lauramurgatroyd@users.noreply.github.com>
2021-08-05 11:02:53 +0100

    add radio signals and functions

--- main.ts
+++ main.ts
@@ -1,27 +1,42 @@
-let moving = 0
+let moving = false
+function stop_moving () {
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . . . . .
+        # . . . #
+        . # # # .
+        `)
+    kitronik_klip_motor.motorOff(kitronik_klip_motor.Motors.Motor1)
+    kitronik_klip_motor.motorOff(kitronik_klip_motor.Motors.Motor2)
+    moving = false
+}
 input.onButtonPressed(Button.A, function () {
-    if (moving == 1) {
-        basic.showLeds(`
-            . . . . .
-            . # . # .
-            . . . . .
-            # . . . #
-            . # # # .
-            `)
-        kitronik_klip_motor.motorOff(kitronik_klip_motor.Motors.Motor1)
-        kitronik_klip_motor.motorOff(kitronik_klip_motor.Motors.Motor2)
-        moving = 0
+    if (moving == true) {
+        stop_moving()
     } else {
-        basic.showLeds(`
-            . . . . .
-            . # . # .
-            . . . . .
-            # . . . #
-            . # # # .
-            `)
-        kitronik_klip_motor.motorOn(kitronik_klip_motor.Motors.Motor1, kitronik_klip_motor.MotorDirection.Forward, 100)
-        kitronik_klip_motor.motorOn(kitronik_klip_motor.Motors.Motor2, kitronik_klip_motor.MotorDirection.Forward, 100)
-        moving = 1
+        stop_moving()
+    }
+})
+function reverse () {
+    kitronik_klip_motor.motorOn(kitronik_klip_motor.Motors.Motor1, kitronik_klip_motor.MotorDirection.Reverse, 100)
+    kitronik_klip_motor.motorOn(kitronik_klip_motor.Motors.Motor2, kitronik_klip_motor.MotorDirection.Reverse, 100)
+}
+function turn_left () {
+    kitronik_klip_motor.motorOn(kitronik_klip_motor.Motors.Motor1, kitronik_klip_motor.MotorDirection.Forward, 100)
+    kitronik_klip_motor.motorOff(kitronik_klip_motor.Motors.Motor2)
+    basic.pause(1000)
+    kitronik_klip_motor.motorOn(kitronik_klip_motor.Motors.Motor1, kitronik_klip_motor.MotorDirection.Forward, 100)
+}
+radio.onReceivedString(function (receivedString) {
+    if (receivedString == "go") {
+        start_moving()
+    } else if (receivedString == "stop") {
+        stop_moving()
+    } else if (receivedString == "reverse") {
+        reverse()
+    } else if (receivedString == "turn") {
+        turn_left()
     }
 })
 input.onButtonPressed(Button.B, function () {
@@ -62,3 +77,15 @@ input.onButtonPressed(Button.B, function () {
     music.playTone(392, music.beat(BeatFraction.Whole))
     music.playTone(349, music.beat(BeatFraction.Double))
 })
+function start_moving () {
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . . . . .
+        # . . . #
+        . # # # .
+        `)
+    kitronik_klip_motor.motorOn(kitronik_klip_motor.Motors.Motor1, kitronik_klip_motor.MotorDirection.Forward, 100)
+    kitronik_klip_motor.motorOn(kitronik_klip_motor.Motors.Motor2, kitronik_klip_motor.MotorDirection.Forward, 100)
+    moving = true
+}
